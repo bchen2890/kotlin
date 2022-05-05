f7587ada18baa3be5be3634ad33a42562ae5d59e
bchen2890 <99276640+bchen2890@users.noreply.github.com>
2022-05-05 23:35:37 +0200

    Fixed

--- src/main/kotlin/leetCodeProblem/TwoSum.kt
+++ src/main/kotlin/leetCodeProblem/TwoSum.kt
@@ -10,17 +10,14 @@ class TwoSum {
      * Brute Force Solution. Complexity O(n^2)
      */
     fun solutionBruteForce(nums: IntArray, target: Int): IntArray {
-        var res = IntArray(2);
-
         for (i in nums.indices) {
             for (j in i + 1 until nums.size) {
                 if (nums[j] == target - nums[i]) {
-                    res = intArrayOf(i, j);
-                    break;
+                    return intArrayOf(i, j);
                 }
             }
         }
-        return res
+        return intArrayOf(-1,-1)
     }
 
     /**
